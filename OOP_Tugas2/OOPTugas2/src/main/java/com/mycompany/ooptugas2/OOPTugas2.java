e02c7740ab9d1f5cb10e1e458e459befb775a60e
Achmad Madania Hasan Mansur <madania@contoh.com>
2024-08-26 20:01:42 +0700

    Added two exercises

---
+++ OOP_Tugas2/OOPTugas2/src/main/java/com/mycompany/ooptugas2/OOPTugas2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ooptugas2;
+
+/**
+ *
+ * @author Acer
+ */
+public class OOPTugas2 {
+    public static void main(String[] args) {
+        System.out.println("Achmad Madania Hasan Mansur");
+        System.out.println("2340506062");
+        System.out.println("Krajan, Magelang");
+        System.out.println("089123456789");
+    }
+}
